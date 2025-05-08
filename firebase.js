bb9dcc6d25f8b93accda4180f193f139c3ba02a5
Vaibhav Kumawat <vaibhavkumawat474@gmail.com>
2025-05-08 15:51:27 +0530

    firbase file updated

--- firebase.js
+++ firebase.js
@@ -1,5 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+if (!process.env.GOOGLE_SERVICE_ACCOUNT_KEY) {
+  throw new Error(
+    "Missing GOOGLE_SERVICE_ACCOUNT_KEY in environment variables"
+  );
+}
+
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
