afd4b0d0a40ff5f598d22430dc2de4a48d7ab183
Vaibhav Kumawat <vaibhavkumawat474@gmail.com>
2025-05-22 18:39:03 +0530

    code updated

--- Controller/propertyController.js
+++ Controller/propertyController.js
@@ -1,5 +1,6 @@
 const Property = require("../Model/propertyModel");
 const AppError = require("../Utils/AppError");
+const ApiFeatures = require("../Utils/ApiFeatures");
 
 exports.createProperty = async (req, res, next) => {
   try {
@@ -15,13 +16,19 @@ exports.createProperty = async (req, res, next) => {
 
 exports.getAllProperties = async (req, res, next) => {
   try {
-    const properties = await Property.find().sort("-createdAt");
-    res.status(201).json({
+    const features = new ApiFeatures(Property.find(), req.query)
+      .filter()
+      .sort()
+      .limitFeilds()
+      .page();
+    const properties = await features.query;
+    res.status(200).json({
       status: "success",
       totalProperties: properties.length,
       message: properties,
     });
   } catch (error) {
+    console.log(error);
     return next(new AppError(error.message, 500));
   }
 };
---
+++ Utils/ApiFeatures.js
@@ -0,0 +1,48 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObject = { ...this.queryString };
+    const excludedFeilds = ["page", "sort", "limit", "feilds"];
+    excludedFeilds.forEach((el) => delete queryObject[el]);
+    let queryStr = JSON.stringify(queryObject);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+
+  limitFeilds() {
+    if (this.queryString.fields) {
+      console.log(this.queryString);
+      const fields = this.queryString.fields.split(",").join(" ");
+      console.log(fields);
+      this.query = this.query.select("propertyName");
+    } else {
+      this.query = this.query.select("-__v");
+    }
+    return this;
+  }
+
+  page() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+module.exports = ApiFeatures;
